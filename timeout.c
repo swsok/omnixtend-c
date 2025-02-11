e5e567f8d93704b7160866c4722aeedc8d1d8675
seungjunn <seungjunn@gmail.com>
2025-02-11 16:18:01 +0900

    Change the timer to operate in us units. bug fixed

--- timeout.c
+++ timeout.c
@@ -1,21 +1,28 @@
 #include "timeout.h"
 
+long get_current_time() {
+	struct timespec ts;
+	clock_gettime(CLOCK_MONOTONIC, &ts);
+	return (ts.tv_sec * 1000000) + (ts.tv_nsec / 1000);
+}
+
 int is_timeout_tx(time_t send_time) {
 	return difftime(time(NULL), send_time) >= TIMEOUT_TX;
 }
 
 void init_timeout_rx(TimeoutRX *rx) {
-    rx->last_ack_time = time(NULL);
+	rx->last_ack_time = get_current_time();
     rx->last_ack_seq = -1;
     rx->ack_pending = 0;
 }
 
 int is_send_delayed_ack(TimeoutRX *rx) {
-    time_t now = time(NULL);
+	long curr_ms = get_current_time();
+	long elapsed_ms = curr_ms - rx->last_ack_time;
 
-    if (rx->ack_pending && (now - rx->last_ack_time) >= TIMEOUT_RX) {
-        return 1;
-    }
-    return 0;
+	if (rx->ack_pending && elapsed_ms >= TIMEOUT_RX) {
+		return 1;
+	}
+	return 0;
 }
 
--- timeout.h
+++ timeout.h
@@ -2,15 +2,16 @@
 #define __TIMEOUT_H__
 #include <time.h>
 
-#define TIMEOUT_TX 2   // RTT * 2
-#define TIMEOUT_RX 1   // RTT * 2
+#define TIMEOUT_TX 1000   // RTT * 2 (unit:us)
+#define TIMEOUT_RX 100     // RTT * 2 (unit:us)
 
 typedef struct {
-    time_t last_ack_time;
+	long last_ack_time;
     int last_ack_seq;
     int ack_pending;
 } TimeoutRX;
 
+long get_current_time();
 int is_timeout_tx(time_t send_time);
 void init_timeout_rx(TimeoutRX *rx);
 int is_send_delayed_ack(TimeoutRX *rx);
