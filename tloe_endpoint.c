e5a7ce1d752186492ed0644eff3becec0221f712
Kwangwon Koh <kwangwon.koh@etri.re.kr>
2025-02-24 08:36:49 +0900

    Enhance endpoint status printing with more detailed information
    - Add estimated ACK count on the other side to status output
    - Consolidate print statements for more compact and readable status display

--- tloe_endpoint.c
+++ tloe_endpoint.c
@@ -141,26 +141,26 @@ void *tloe_endpoint(void *arg) {
 }
 
 static void print_endpoint_status(tloe_endpoint_t *e) {
-    printf("-----------------------------------------------------\n");
-    printf("Sequence Numbers:\n");
-    printf(" TX: %d, RX: %d\n", e->next_tx_seq, e->next_rx_seq);
-    
-    printf("\nPacket Statistics:\n");
-    printf(" ACK: %d, Duplicate: %d, Out-of-Sequence: %d\n", 
-           e->ack_cnt, e->dup_cnt, e->oos_cnt);
-    printf(" Delayed: %d, Dropped: %d (Normal: %d, ACK: %d)\n",
-           e->delay_cnt, e->drop_cnt, e->drop_npacket_cnt, e->drop_apacket_cnt);
-    
-    printf("\nChannel Credits [A|B|C|D|E]: %d|%d|%d|%d|%d\n",
+    printf("-----------------------------------------------------\n"
+           "Sequence Numbers:\n"
+           " TX: %d, RX: %d\n"
+           "\nPacket Statistics:\n"
+           " ACK: %d, Duplicate: %d, Out-of-Sequence: %d\n"
+           " Delayed: %d, Dropped: %d (Normal: %d, ACK: %d)\n"
+	   " Estimated ACK on the other side: %d\n"
+           "Channel Credits [A|B|C|D|E]: %d|%d|%d|%d|%d\n"
+           " Flow Control (Inc/Dec): %d/%d\n"
+           "\nBuffer Drops:\n"
+           " TL Messages: %d, Responses: %d\n"
+           "-----------------------------------------------------\n",
+           e->next_tx_seq, e->next_rx_seq,
+           e->ack_cnt, e->dup_cnt, e->oos_cnt,
+           e->delay_cnt, e->drop_cnt, e->drop_npacket_cnt, e->drop_apacket_cnt,
+	   e->next_rx_seq-e->delay_cnt+e->oos_cnt+e->dup_cnt-e->drop_apacket_cnt,
            e->fc.credits[CHANNEL_A], e->fc.credits[CHANNEL_B], 
            e->fc.credits[CHANNEL_C], e->fc.credits[CHANNEL_D], 
-           e->fc.credits[CHANNEL_E]);
-    printf(" Flow Control (Inc/Dec): %d/%d\n", e->fc_inc_cnt, e->fc_dec_cnt);
-    
-    printf("\nBuffer Drops:\n");
-    printf(" TL Messages: %d, Responses: %d\n", 
+           e->fc.credits[CHANNEL_E], e->fc_inc_cnt, e->fc_dec_cnt,
            e->drop_tlmsg_cnt, e->drop_response_cnt);
-    printf("-----------------------------------------------------\n");
 }
 
 static int create_and_enqueue_message(tloe_endpoint_t *e, int msg_index) {
